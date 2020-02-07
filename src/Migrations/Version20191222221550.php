71202ffadd4350fdeede0b44858272d6f82dfab2
unknown <araba.mohamed@gmail.com>
2020-02-07 03:26:25 +0100

    Correction d'un beug, dans un des fichiers de migrations

--- src/Migrations/Version20191222221550.php
+++ src/Migrations/Version20191222221550.php
@@ -22,7 +22,9 @@ final class Version20191222221550 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE ad ADD author_id INT NOT NULL');
         $this->addSql('ALTER TABLE ad ADD CONSTRAINT FK_77E0ED58F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        
         $this->addSql('CREATE INDEX IDX_77E0ED58F675F31B ON ad (author_id)');
     }
 
@@ -33,5 +35,6 @@ final class Version20191222221550 extends AbstractMigration
 
         $this->addSql('ALTER TABLE ad DROP FOREIGN KEY FK_77E0ED58F675F31B');
         $this->addSql('DROP INDEX IDX_77E0ED58F675F31B ON ad');
+        $this->addSql('ALTER TABLE ad DROP author_id');
     }
 }
